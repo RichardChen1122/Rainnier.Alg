0ce4db2ee3b6137aa29996df3e7acf2537e4e0a4
RichardChen1122 <rchen89@126.com>
2020-06-29 21:20:18 +0800

    Add Leecode 1465 and 801 solution

---
+++ Rainnier.Alg/arr/Leetcode1465.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.Alg.arr
+{
+    class Leetcode1465
+    {
+        public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+        {
+            long max1 = FindMaxSpan(horizontalCuts, h);
+            long max2 = FindMaxSpan(verticalCuts, w);
+
+            long test = (max1 * max2)%(10000*100000+7);
+            return (int)test;
+        }
+
+        public int FindMaxSpan(int[] array, int k)
+        {
+            QuickSort(array,0,array.Length-1);
+            int result = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                result = Math.Max(result, array[i] - array[i - 1]);
+            }
+
+            result = Math.Max(result, k - array[array.Length - 1]);
+
+            return result;
+        }
+
+        public void QuickSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                var mid = QuickSortOnce(array, start, end);
+                QuickSort(array, start, mid);
+                QuickSort(array, mid + 1, end);
+            }
+        }
+
+        public int QuickSortOnce(int[] array, int start, int end)
+        {
+            int temp = array[start];
+
+            while (start < end)
+            {
+                while(array[end]>=temp && start < end)
+                {
+                    end--;
+                }
+                array[start] = array[end];
+                while (array[start] < temp && start < end)
+                {
+                    start++;
+                }
+                array[end] = array[start];
+            }
+
+            array[start] = temp;
+
+            return start;
+        }
+    }
+}
---
+++ Rainnier.Alg/arr/Leetcode801.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.Alg.arr
+{
+    class Leetcode801
+    {
+        public int MinSwap(int[] A, int[] B)
+        {
+            int i = 1;
+            int nochange = 0;
+            int change = 1;
+            while (i < A.Length)
+            {
+                if (A[i - 1] < A[i] && B[i - 1] < B[i]) //这种情况要根据前一次的交换情况进行判断
+                {
+                    if (A[i - 1] >= B[i] || B[i - 1] >= A[i])//这种情况下，要么不交换 nochange 不变。如果交换，必须基于前一次交换的基础上一起交换，所以要change++
+                    {
+                        change++;
+                    }
+                    else //这种情况下， 如果交换，前一次交换不交换都无所谓，找小的+1即可，如果不叫唤，那迁移此叫不交换也无所谓， 取小的即可
+                    {
+                        int temp = change;
+                        change = Math.Min(nochange, change) + 1;
+                        nochange = Math.Min(nochange, temp);
+                    }
+                }
+                else //由于输入的结果一定是有效的，所以其他情况只能是 A[i-1]<B[i] && B[i-1]<A[i]
+                {
+                    int temp = change;
+                    change = nochange + 1;
+                    nochange = temp;
+
+                }
+                i++;
+            }
+
+            return Math.Min(nochange, change);
+        }
+    }
+}
