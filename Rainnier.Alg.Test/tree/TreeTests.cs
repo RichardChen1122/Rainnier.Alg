900e9ccf4dfeb685197d38a164b60945f45e95d8
Richard Chen <junbchen@microsoft.com>
2023-07-07 23:38:49 +0800

    Add node search

--- Rainnier.Alg.Test/tree/TreeTests.cs
+++ Rainnier.Alg.Test/tree/TreeTests.cs
@@ -6,25 +6,34 @@ using Rainnier.Alg;
 using Rainnier.Alg.arr;
 using Rainnier.Alg.leetcode.LCP;
 using Rainnier.Alg.str;
+using Rainnier.Alg.tree;
 using Rainnier.Alg.tree.model;
 
 
 namespace Rainnier.Alg.Test.tree
 {
     [TestClass]
-    internal class TreeTests
+    public class TreeTests
     {
-        private TreeNode root;
-
         [TestMethod]
-        public void Leetcode214()
+        public void NodeSerach()
         {
-            var leetcode214 = new Leetcode214();
-            Assert.AreEqual(leetcode214.ShortestPalindrome("abc"), "cbabc");
-            Assert.AreEqual(leetcode214.ShortestPalindrome("abba"), "abba");
-            Assert.AreEqual(leetcode214.ShortestPalindrome("aacecaaa"), "aaacecaaa");
-            Assert.AreEqual(leetcode214.ShortestPalindrome(""), "");
-            Assert.AreEqual(leetcode214.ShortestPalindrome("a"), "a");
+            TreeNode root;
+
+            TreeNode searchNode;
+            var c = new NodeSearch();
+            root = new TreeNode(10);
+            var node1 = root.left = new TreeNode(5);
+            root.right = new TreeNode(14);
+            node1.left = new TreeNode(1);
+            var node4 = node1.right = new TreeNode(8);
+            node4.left = new TreeNode(6);
+            searchNode = node4.right = new TreeNode(9);
+
+            var stack = new Stack<TreeNode>();
+            c.NodeSearchMethod(root, stack, searchNode);
+
+            Assert.AreEqual(4, stack.Count);
         }
     }
 }
---
+++ Rainnier.Alg/tree/NodeSearch.cs
@@ -0,0 +1,40 @@
+﻿using Rainnier.Alg.tree.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.Alg.tree
+{
+    public class NodeSearch
+    {
+        public bool NodeSearchMethod(TreeNode root, Stack<TreeNode> stack, TreeNode searchNode)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            //#1放这里也可以
+
+            var left = NodeSearchMethod(root.left, stack, searchNode);
+            var right = NodeSearchMethod(root.right, stack, searchNode);
+
+            //这段代码#1放上面也可以--------------
+            if (root == searchNode)
+            {
+                stack.Push(root);
+                return true;
+            }
+            //-----------------------------------
+
+            if (left || right)
+            {
+                stack.Push(root);
+                return true;
+            }
+            return false;
+        }
+    }
+}
